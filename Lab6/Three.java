d763bbada0e1d7630c8c647b6621697e7e41e9c3
Abbie Guest <abguest@iu.edu>
2020-01-22 19:01:52 -0500

    Add files via upload

---
+++ Lab6/Three.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Three {	
+	
+	public static void main(String[] args) {
+		int[] numbers=new int[0];
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter: ");
+		String line=s.nextLine();
+		while (! line.equals("bye")) {
+			int number=Integer.parseInt(line);
+			numbers=Three.copyOf(numbers, numbers.length+1);
+			numbers[numbers.length-1]=number;
+			System.out.println(Three.toString(numbers));
+			System.out.print("Enter: ");
+			line=s.nextLine();
+		}
+		Three.sort(numbers);
+		System.out.println(Three.toString(numbers));
+		s.close();
+}
+	public static String toString(int[] a) {
+		String result="";
+		for (int i=0; i<a.length; i++) 
+			result=result + a[i]+ ", ";
+		if (a.length>0) 
+			result=result.substring(0, result.length()-2);	
+			
+		
+		return "[" + result + "]";	
+	}
+	
+	public static int[] copyOf(int[] a, int size) {
+		int[] b= new int[size];
+		for (int i=0; i < a.length && i<size; i++) {
+			b[i]=a[i];
+		}
+		return b;		
+	}
+	
+	public static void sort(int[] a) {
+		for (int i=0; i<a.length; i++) {
+			for (int j=0; j < a.length-1; j++)
+			if (a[j] > a[j+1]) {
+				int temp= a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+				
+			}
+		}
+		
+	}
+}
